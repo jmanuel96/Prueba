cb551b4429db9f4a1a69305fbb952f2838b1a675
Jose Manuel <vivaerbetismakina@gmail.com>
2019-11-12 13:07:26 +0100

    Primero

---
+++ src/app/becoven.ts
@@ -0,0 +1,7 @@
+export interface Becoven {
+    titulo: string;
+    descripcion: string;
+    ingredientes: string;
+    precio: number;
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { FirestoreService } from '../firestore.service';
+import { Becoven } from '../becoven';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +10,66 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  becovenEditando: Becoven;
+
+  arrayColeccionBecoven: any = [{
+    id: "",
+    data: {} as Becoven
+   }];
+
+  constructor(private firestoreService: FirestoreService) {
+    // Crear un becoven vacío
+    this.becovenEditando = {} as Becoven;
+    this.obtenerListaBecoven();
+  }
+
+  clicBotonInsertar() {
+    this.firestoreService.insertar("productos", this.becovenEditando).then(() => {
+      console.log('Producto creado correctamente!');
+      this.becovenEditando= {} as Becoven;
+    }, (error) => {
+      console.error(error);
+    });
+  }
+  obtenerListaBecoven(){
+    this.firestoreService.consultar("productos").subscribe((resultadoConsultaBecoven) => {
+      this.arrayColeccionBecoven = [];
+      resultadoConsultaBecoven.forEach((datosBecoven: any) => {
+        this.arrayColeccionBecoven.push({
+          id: datosBecoven.payload.doc.id,
+          data: datosBecoven.payload.doc.data()
+        });
+      })
+    });
+  }
+
+  idBecovenSelec: string;
+
+  selecBecoven(becovenSelec) {
+    console.log("Producto seleccionado: ");
+    console.log(becovenSelec);
+    this.idBecovenSelec = becovenSelec.id;
+    this.becovenEditando.titulo = becovenSelec.data.titulo;
+    this.becovenEditando.descripcion = becovenSelec.data.descripcion;
+    this.becovenEditando.precio = becovenSelec.data.precio;
+  }
+
+  clicBotonBorrar() {
+    this.firestoreService.borrar("productos", this.idBecovenSelec).then(() => {
+      // Actualizar la lista completa
+      this.obtenerListaBecoven();
+      // Limpiar datos de pantalla
+      this.becovenEditando = {} as Becoven;
+    })
+  }
+
+  clicBotonModificar() {
+    this.firestoreService.actualizar("productos", this.idBecovenSelec, this.becovenEditando).then(() => {
+      // Actualizar la lista completa
+      this.obtenerListaBecoven();
+      // Limpiar datos de pantalla
+      this.becovenEditando = {} as Becoven;
+    })
+  }
 
 }
