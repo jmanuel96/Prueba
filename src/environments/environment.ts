26eba1cdc71212ace8e67f467004c34ae4a8b369
jmanuel96 <46520127+jmanuel96@users.noreply.github.com>
2019-11-13 14:28:53 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,6 @@
 export const environment = {
   production: false,
   firebase:{
-    apiKey: "AIzaSyAZqGvnE6x-sJY1Mpyf7MpkVVMhxMaMySM",
-    authDomain: "bbdd-841fb.firebaseapp.com",
-    databaseURL: "https://bbdd-841fb.firebaseio.com",
-    projectId: "bbdd-841fb",
-    storageBucket: "bbdd-841fb.appspot.com",
-    messagingSenderId: "968345370599",
-    appId: "1:968345370599:web:5bfac9ad06ab98fe264742"
   }
 };
 
